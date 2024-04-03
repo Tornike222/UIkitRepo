bae0b9f70bde9be4dadd743050fa1254985e3270
tornike222 <tornike.elkanishvili.1@btu.edu.ge>
2024-04-03 08:47:00 +0400

    Initial Commit

---
+++ IntroToiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntroToiOS
+//
+//  Created by telkanishvili on 03.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
