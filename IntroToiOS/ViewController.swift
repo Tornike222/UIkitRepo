b58c57101a7b629e9ad512ac45e4623f566e2eb7
tornike222 <tornike.elkanishvili.1@btu.edu.ge>
2024-04-03 22:51:38 +0400

    add hw12's files to project

--- IntroToiOS/ViewController.swift
+++ IntroToiOS/ViewController.swift
@@ -8,12 +8,94 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var controllerView: UIView!
+    @IBOutlet weak var labelOfUsgUsj: UILabel!
+    @IBOutlet weak var toggleOfUsgUsj: UISwitch!
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var secondTextField: UITextField!
+    @IBOutlet weak var calculateButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        calculateButton.tintColor = UIColor.purple
     }
-
-
+    func resetLabels(){
+        firstTextField.text = ""
+        secondTextField.text = ""
+        resultLabel.text = ""
+    }
+    func getRedCornerOn(field: UITextField){
+        field.layer.borderColor = UIColor.red.cgColor
+        field.layer.borderWidth = 1.0
+        field.layer.cornerRadius = 5.0
+    }
+    //უმცირესი საერთო ჯერადი
+    func getUsj(firstInt: Int, secondInt: Int) -> Int{
+        let maxInt = max(firstInt,secondInt)
+        let minInt = min(firstInt,secondInt)
+        var usj = minInt
+    
+        for _ in 0...maxInt{
+            if usj % maxInt != 0 {
+                usj += minInt
+            }
+        }
+        return usj
+    }
+    //უდიდესი საერთო გამყოფი
+    func getUsg(firstInt: Int, secondInt: Int) -> Int{
+        (firstInt * secondInt) / getUsj(firstInt: firstInt, secondInt: secondInt)
+    }
+    
+    @IBAction func switchClicked(_ sender: UISwitch) {
+        if sender.isOn {
+            labelOfUsgUsj.text = "უდიდესი საერთო გამყოფი"
+            calculateButton.tintColor = UIColor.red
+            resetLabels()
+        } else {
+            labelOfUsgUsj.text = "უმცირესი საერთო ჯერადი"
+            calculateButton.tintColor = UIColor.purple
+            resetLabels()
+        }
+    }
+    
+    @IBAction func calculateButton(_ sender: UIButton) {
+        self.resultLabel.isHidden = false //ერორი რომ დაიმალება და იუზერი დააკლიკებს ბათონს რესალთი ისევ ხელმისაწვდომი რომ იყოს
+        let firstInt = Int(firstTextField.text!) ?? 0
+        let secondInt = Int(secondTextField.text!) ?? 0
+        
+        if firstInt > 0 && secondInt > 0{
+            resultLabel.textColor = UIColor(named: "ResultColor")
+            if toggleOfUsgUsj.isOn {
+                resultLabel.text = "პასუხი: \(getUsg(firstInt: firstInt, secondInt: secondInt))"
+            } else {
+                resultLabel.text = "პასუხი: \(getUsj(firstInt: firstInt, secondInt: secondInt))"
+            }
+        } else if self.resultLabel.isHidden == false {
+            resultLabel.textColor = .red
+            resultLabel.text = "დაფიქსირდა არასწორად შეყვანილი მნიშვნელობა"
+            calculateButton.isEnabled = false
+            
+            if firstInt == 0 && secondInt == 0{
+                getRedCornerOn(field: firstTextField)
+                getRedCornerOn(field: secondTextField)
+                
+            } else if firstInt == 0 {
+                getRedCornerOn(field: firstTextField)
+                
+                
+            } else if secondInt == 0 {
+                getRedCornerOn(field: secondTextField)
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                self.resultLabel.isHidden = true
+                self.firstTextField.layer.borderWidth = 0.0
+                self.secondTextField.layer.borderWidth = 0.0
+                self.calculateButton.isEnabled = true
+            }
+        }
+    }
 }
-
